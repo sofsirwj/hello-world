0af53ca89a4d406e583546206b4ea03383f68dcc
Marvin Wei <sofsirwj@163.com>
2018-12-25 13:07:46 +0800

    add polyfills for supporting ie9-11

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// This must be the first line in src/index.js
+import 'react-app-polyfill/ie9';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
